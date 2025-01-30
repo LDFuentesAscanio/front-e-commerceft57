41517cba56724353f8f797fc129882deec20fd49
LDFuentesAscanio <ludafuasca@gmail.com>
2025-01-30 14:04:25 -0300

    Checkout

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const CardList: React.FC = async () => {
   const productsPreload = await getProductsDB();
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 max-w-7xl mx-auto">
+    <div className="flex flex-wrap justify-center gap-4 p-4">
       {productsPreload &&
         productsPreload.map((product) => (
           <Link
